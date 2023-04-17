82f256a06a47edf77bd9cec87738f4b5e6bd09ff
wlkrm <moritz.walker@isw.uni-stuttgart.de>
2023-04-17 13:55:54 +0200

    feat: add_macvlan

--- networking/create_macvlan.sh
+++ networking/create_macvlan.sh
@@ -1,3 +1,6 @@
 sudo ip link add macvlan1 link enp0s31f6 address a6:7b:b1:78:8a:61 type macvlan mode bridge 
 sudo ip link add macvlan2 link enp0s31f6 address a6:7b:b1:78:8a:62 type macvlan mode bridge 
 sudo ip link add macvlan3 link enp0s31f6 address a6:7b:b1:78:8a:63 type macvlan mode bridge
+sudo ip link set dev macvlan1 up
+sudo ip link set dev macvlan2 up
+sudo ip link set dev macvlan3 up
